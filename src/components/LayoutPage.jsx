7cd3a518537d4f3706ca64ae8504df93a00f31e9
yohannes <yohannestagel21@gmail.com>
2026-08-04 21:20:31 +0300

    i update the layoutPage to be function

--- src/components/LayoutPage.jsx
+++ src/components/LayoutPage.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import HeaderPage from './HeaderPage'
 import FooterPage from './FooterPage'
+import { Outlet, useLocation } from 'react-router-dom'
 
 function LayoutPage() {
+  const {pathname} = useLocation()
   return (
     <div>
     <HeaderPage />
-    <h1 className='text-3xl font-bold'>Layout Page</h1>
-    <p>Welcome to the Layout page!</p>
-    <p>This is a layout component that can be used to wrap other pages.</p>
-    <p>It includes a header and footer.</p>
-    <p>You can add more content here as needed.</p>
-
-    <FooterPage />
-      
+          <main id="main-content" className="flex-1">
+        <div className="mx-auto max-w-6xl px-4 py-12 lg:px-8">
+          <Outlet key={pathname} />
+        </div>
+      </main>
+    <FooterPage /> 
     </div>
   )
 }
