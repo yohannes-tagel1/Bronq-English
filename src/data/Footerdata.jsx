c625d205f258b606357a4d5ada36776ae03e2d1b
yohannes <yohannestagel21@gmail.com>
2026-08-25 10:44:00 +0300

    i update footer

---
+++ src/data/Footerdata.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Services", path: "/services" },
+  { name: "Blog", path: "/blog" },
+  { name: "Get a Quote", path: "/order" },
+  { name: "Contact", path: "/contact" },
+];
+
+
+export const footerfirst = [
+  {
+    title: "Bronq English",
+    herotitle: "Learn English with Confidence",
+    description: "We provide high-quality English language instruction to help you achieve your communication goals."
+    //   heroImageWidth: 500,
+    //   heroImageHeight: 500,
+  }
+];
+
+
+
+export const contact = [
+  {
+    title: "Contact Us",
+    location: "Addis Ababa, Ethiopia",
+    phone: "+251900000",
+    email: "info@bronqenglish.com",
+  }
+];
+
+
+export const conect = [
+  {
+    title: "Connect with Us",
+    facebook: "https://www.facebook.com/bronqenglish",
+    twitter: "https://twitter.com/bronqenglish",
+    instagram: "https://www.instagram.com/bronqenglish",
+    linkedin: "https://www.linkedin.com/company/bronqenglish",
+    telegram:"https://t.me/bronq_english"
+  }
+]   
